40c8f6336db6ab95c58224a3a557ed199810c701
Abdulwahidh2005 <abdulwahidh2005@gmail.com>
2026-09-16 14:38:38 +0530

    First commit

---
+++ day03/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "~> 4.8.0"
+    }
+  }
+  required_version = ">=1.9.0"
+}
+
+provider "azurerm" {
+
+    subscription_id = "46b70d33-6052-422c-86a1-26fd1ebfc601"
+
+    features {
+      
+    }
+  
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "abdulresourcegroup"
+  location = "South India"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "stabdul460926"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
---
+++ day03/var.sh
@@ -0,0 +1,2 @@
+46b70d33-6052-422c-86a1-26fd1ebfc601
+az ad sp create-for-rbac -n az-demo --role="Contributor" --scopes="/subscriptions/46b70d33-6052-422c-86a1-26fd1ebfc601"
